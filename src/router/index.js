716b638424f5c7ac43d29520ced23bcbb8211384
谢翔 <1972524359@qq.com>
2022-02-25 20:58:13 +0800

    路由重构

--- src/router/index.js
+++ src/router/index.js
@@ -2,19 +2,58 @@ import Vue from "vue";
 import VueRouter from "vue-router"
 import MusicList from "../components/MusicList.vue";
 import PlayList2 from "../components/PlayList2.vue";
-
+import LoginRegister from "../components/LoginRegister.vue";
 
 Vue.use(VueRouter);
 
-const originalPush = VueRouter.prototype.push
-VueRouter.prototype.push = function push(location) {
-  return originalPush.call(this, location).catch(err => err)
-}
-
-export default new VueRouter({
+const router = new VueRouter({
   routes: [
-    { path: "/search", components: { MusicList } },
-    { path: "/playlist/track/all", components: { MusicList } },
-    { path: "/myMusic", components: { PlayList2 } },
+    {
+      path: "/top",
+      name: "top",
+      component: { PlayList2 },
+    },
+    {
+      path: "/top/playList",
+      name: "tplayList",
+      components: { MusicList }
+    },
+    {
+      path: "/myMusic",
+      name: "myMusic",
+      meta: { requireAuth: true },
+      components: { PlayList2 },
+    },
+    {
+      path: "/myMusic/playList",
+      name: "mplayList",
+      components: { MusicList }
+    },
+    {
+      path: "/search",
+      name: "search",
+      components: { MusicList }
+    },
+    {
+      path: "/login",
+      name: "login",
+      components: { LoginRegister }
+    },
   ]
 });
+
+// 守卫当前路由是否需要授权
+router.beforeEach((to, from, next) => {
+  console.log(to, from);
+  if (to.meta.requireAuth) {
+    if (localStorage.getItem("authorization")) {
+      next();
+    } else {
+      next("/login");
+    }
+  } else {
+    next();
+  }
+});
+
+export default router;
